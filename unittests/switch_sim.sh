1d1c9af9002d3b869816cc03fd2f9a7c193c1b7b
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-07-28 04:47:12 -0400

    Add more unit tests

--- unittests/switch_sim.sh
+++ unittests/switch_sim.sh
@@ -2,3 +2,9 @@
 
 ./src/switch_sim 64 1 10000 1 128 > /tmp/output
 diff /tmp/output unittests/64.1.10000.output
+
+./src/switch_sim 64 1 10000 64 3 > /tmp/output
+diff /tmp/output unittests/64.1.10000.output
+
+./src/switch_sim 64 1 10000 64 2 > /tmp/output
+diff /tmp/output unittests/64.1.10000.output
