70528c6a04efad269c906168d05b9a4843e2239d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-08-02 17:04:22 -0400

    Interchange input and output, it's confusing otherwise. Now, traffic generation also makes sense. Earlier, we were generating a random input port for every output port. It should be the other way around, otherwise even 2 operations per tick for every memory bank is sufficient.

--- src/cell.cc
+++ src/cell.cc
@@ -2,6 +2,6 @@
 
 #include "src/cell.h"
 
-Cell::Cell(const uint16_t s_output, const uint16_t s_input)
-    : output_(s_output),
-      input_(s_input) {}
+Cell::Cell(const uint16_t s_input, const uint16_t s_output)
+    : input_(s_input),
+      output_(s_output) {}
--- src/cell.h
+++ src/cell.h
@@ -7,13 +7,13 @@
 
 class Cell {
  public:
-  Cell(const uint16_t s_output, const uint16_t s_input);
+  Cell(const uint16_t s_input, const uint16_t s_output);
   Cell() {}
-  uint16_t output(void) const { return output_; }
   uint16_t input(void) const { return input_; }
+  uint16_t output(void) const { return output_; }
  private:
-  uint16_t output_ {0};
   uint16_t input_ {0};
+  uint16_t output_ {0};
 };
 
 #endif  // SRC_CELL_H_
