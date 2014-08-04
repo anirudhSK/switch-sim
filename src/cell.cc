211d4d2824ca1779fbde6f5b39b25dc828772abf
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-08-03 20:21:03 -0400

    Assert that you have no loopback cells.

--- src/cell.cc
+++ src/cell.cc
@@ -1,7 +1,9 @@
 // Copyright 2014 Anirudh Sivaraman
 
+#include <cassert>
+
 #include "src/cell.h"
 
 Cell::Cell(const uint16_t s_input, const uint16_t s_output)
     : input_(s_input),
-      output_(s_output) {}
+      output_(s_output) { assert(input_ != output_); }
