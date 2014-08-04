8edf7e6335770ef197d7fd4d86c9ac5a5956cd83
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-08-03 21:46:16 -0400

    Update unittests, thankfully 2 operations per tick fails now.

--- unittests/switch_sim.sh
+++ unittests/switch_sim.sh
@@ -5,6 +5,3 @@ diff /tmp/output unittests/64.1.10000.output
 
 ./src/switch_sim 64 1 10000 64 3 > /tmp/output
 diff /tmp/output unittests/64.1.10000.output
-
-./src/switch_sim 64 1 10000 64 2 > /tmp/output
-diff /tmp/output unittests/64.1.10000.output
