407401d47c32126dff9e9b6d5ebae0c70a84e43d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-12-12 16:06:06 -0800

    Clean up code to make it more understandable

--- load_balancing.py
+++ load_balancing.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python
 # Simulate Keslassy et al's paper on Optimal Load Balancing
-import random
 import numpy.random
 import sys
 
@@ -11,11 +10,10 @@ SPINES = PORTS
 LINE_RATE = PORTS
 # i.e. rate relative to interconnect links
 # Keslassy's paper sets the line rates to R
-# and the interconnect rates to R/N, we set
-# R to N, so that R/N = 1
-# (so that we don't deal with frac. rates)
+# and the interconnect rates to R/N (Fig. 1).
+# We set R=N, so that R/N = 1
+# (so that we don't use fractional rates)
 ARRIVAL_RATE = float(sys.argv[2])
-
 leaf_nodes = range(LEAFS)
 spine_nodes = range(SPINES)
 
@@ -23,8 +21,11 @@ spine_nodes = range(SPINES)
 leaf_inputs  = [] 
 leaf_outputs = []
 spine_voqs = []
+
+# Stat counters
 output_pkt_count = []
 output_del_acc = []
+
 for leaf in leaf_nodes:
   leaf_inputs.append([])
   leaf_outputs.append([])
@@ -40,43 +41,39 @@ for spine in spine_nodes:
 for current_tick in range(0, TICKS):
   # Generate packets at leaf_inputs
   for i in range(0, LEAFS):
-    pkts_this_slot = numpy.random.binomial(LINE_RATE, ARRIVAL_RATE);
-    for j in range(0, pkts_this_slot):
-      leaf_inputs[i].append((current_tick, random.randint(0, LEAFS - 1)));
+    for j in range(0, numpy.random.binomial(LINE_RATE, ARRIVAL_RATE)):
+      leaf_inputs[i].append((current_tick, numpy.random.random_integers(0, LEAFS - 1)));
 
-  # Move them from leaf_inputs to spine_voqs
-  # Use round-robin to go through all spines
-  # full mesh from leafs to spines
+  # Move all packets from leaf_inputs to spine_voqs
+  # Use round-robin to load balance across all spines
+  # We have a full mesh from leafs to spines
+  # and each leaf-spine link can transmit 1 pkt/slot
   spine_cursor=0
   for i in range(0, LEAFS):
-    for j in range(0, SPINES):
-      if (len(leaf_inputs[i]) == 0):
-        continue
+    while (len(leaf_inputs[i]) > 0):
       pkt_to_bounce = leaf_inputs[i].pop(0);
       spine_voqs[spine_cursor][pkt_to_bounce[1]].append(pkt_to_bounce);
       spine_cursor = (spine_cursor + 1) % SPINES
 
+  # All packets should have been moved by now
   for i in range(0, LEAFS):
     assert(len(leaf_inputs[i]) == 0)
 
-  # Move them from spine_voqs to leaf_outputs
-  # VOQs implicitly do round-robin
-  # full mesh from spines to leafs
+  # Move as many pkts as possible from spine_voqs to leaf_outputs
+  # Use round-robin to go through all VOQs
+  # We have a full mesh from spines to leafs
+  # and each spine-leaf link can transmit 1 pkt/slot
   for i in range(0, SPINES):
     for j in range(0, LEAFS):
-      if (len(spine_voqs[i][j]) == 0):
-        continue
-      pkt_to_send = spine_voqs[i][j].pop(0)
-      assert(pkt_to_send[1] == j);
-      leaf_outputs[pkt_to_send[1]].append(pkt_to_send);
+      if (len(spine_voqs[i][j]) > 0):
+        pkt_to_send = spine_voqs[i][j].pop(0)
+        assert(pkt_to_send[1] == j);
+        leaf_outputs[pkt_to_send[1]].append(pkt_to_send);
 
   # Transmit packets out
   for i in range(0, LEAFS):
-    # pop out LINE RATE number of packets
-    for j in range(0, LINE_RATE):
-     if (len(leaf_outputs[i])==0):
-      break
-     else :
+    # pop out up to LINE RATE number of packets
+    for j in range(0, min(LINE_RATE, len(leaf_outputs[i]))):
       tx_pkt = leaf_outputs[i].pop(0);
       output_pkt_count[i] = output_pkt_count[i] + 1;
       assert(current_tick >= tx_pkt[0]);
@@ -85,7 +82,7 @@ for current_tick in range(0, TICKS):
 # Output stats
 for i in range (0, LEAFS):
   print i, (output_pkt_count[i] * 1.0 / (TICKS * LINE_RATE)), "pkt/tick",\
-         (output_del_acc[i] * 1.0 /output_pkt_count[i]) * LINE_RATE, "ticks"
+        (output_del_acc[i] * 1.0 /output_pkt_count[i]) * LINE_RATE, "ticks"
 
 # Queue stats
 for i in range (0, LEAFS):
