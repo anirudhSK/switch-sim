43c7e815566023afad85d8f55eb6c060b0968d45
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-12-15 12:07:01 -0800

    Transfer on both links, also randomize order of ticking the links

--- convergence.py
+++ convergence.py
@@ -17,39 +17,55 @@ import numpy.random
 import numpy
 
 class SrcNode:
+
   def __init__(self, t_line_rate, t_arrival_rate):
     self.line_rate = t_line_rate
     self.arrival_rate = t_arrival_rate
     self.pkt_queue = []
+
   def tick(self, targets):
     # Generate packets
     for j in range(0, numpy.random.binomial(self.line_rate, self.arrival_rate)):
       self.pkt_queue.append((current_tick, 0))
 
-    # Transfer based on backpressure
-    backpressures = [(target, len(self.pkt_queue) - target.get_queue_size()) for target in targets if (len(self.pkt_queue) - target.get_queue_size() > 0)]
-    if (len(backpressures) > 0) :
-      picked_queue = max(backpressures)[0]
-      print "Picking ", picked_queue
-      assert(picked_queue.get_queue_size() < len(self.pkt_queue))
-      picked_queue.recv(self.pkt_queue.pop(0))
+    # Transfer based on backpressure to destination on each of the links
+    # Since there is only one destination, this is trivial
+    for target in numpy.random.permutation(targets):
+      backpressure = len(self.pkt_queue) - target.get_queue_size()
+      if (backpressure > 0) :
+        print "Picking ", target
+        assert(target.get_queue_size() < len(self.pkt_queue))
+        target.recv(self.pkt_queue.pop(0))
 
 class WayPointNode:
+
   def __init__(self):
     self.pkt_queue = []
+
   def tick(self, targets):
-    print "WayPointNode ticking"
+    assert(len(targets) == 1)
+    if (len(self.pkt_queue) > 0):
+      targets[0].recv(self.pkt_queue.pop(0))
+
   def get_queue_size(self):
     return len(self.pkt_queue)
+
   def recv(self, pkt):
     self.pkt_queue.append(pkt)
 
 class DstNode:
+
+  def __init__(self):
+    self.pkt_queue = []
+
+  def recv(self, pkt):
+    self.pkt_queue.append(pkt)
+
   def tick(self, targets):
-    print "DstNode ticking"
+    pass
 
 LINE_RATE = 2
-ARRIVAL_RATE = 0.8
+ARRIVAL_RATE = 0.1
 srcnode = SrcNode(LINE_RATE, ARRIVAL_RATE)
 waypoint1 = WayPointNode()
 waypoint2 = WayPointNode()
