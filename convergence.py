5b1c4f3c577573e191faed4c2c8cd61dbb394ddf
Anirudh Sivaraman <sk.anirudh@gmail.com>
2014-12-17 16:25:47 -0800

    Tag Destinations with id as well and include destinations in src tick()

--- convergence.py
+++ convergence.py
@@ -3,16 +3,9 @@
 # A simulator to contrast the convergence of backpressure and CONGA
 
 # Here's the setup:
-#        spine1
-#        /     \
-#       /       \
-#      /         \
-#  srcnode     dstnode
-#      \         /
-#       \       /
-#        \     /
-#        spine2
-#
+#           spine1
+#    src1            dst1
+#           spine2
 import numpy.random
 import numpy
 
@@ -23,10 +16,10 @@ class SrcNode:
     self.arrival_rate = t_arrival_rate
     self.pkt_queue = []
 
-  def tick(self, targets, current_tick):
+  def tick(self, targets, current_tick, dsts):
     # Generate packets
     for j in range(0, numpy.random.binomial(self.line_rate, self.arrival_rate)):
-      self.pkt_queue.append((current_tick, 0))
+      self.pkt_queue.append((current_tick, numpy.random.choice(dsts).get_id()))
 
     # Transfer based on backpressure to destination on each of the links
     # Since there is only one destination, this is trivial
@@ -64,27 +57,35 @@ class SpineNode:
 
 class DstNode:
 
-  def __init__(self, t_line_rate):
+  def __init__(self, t_line_rate, t_id):
     self.line_rate = t_line_rate
     self.pkt_queue = []
+    self.id = t_id
 
   def recv(self, pkt, current_tick):
+    assert(pkt[1] == self.id)
     self.pkt_queue.append(pkt)
 
   def tick(self, targets, current_tick):
     for i in range(min(self.line_rate, len(self.pkt_queue))):
       self.pkt_queue.pop(0)
 
+  def get_id(self):
+    return self.id
+
 LINE_RATE = 2
 ARRIVAL_RATE = 0.5
-srcnode = SrcNode(LINE_RATE, ARRIVAL_RATE)
+src1 = SrcNode(LINE_RATE, ARRIVAL_RATE)
 spine1 = SpineNode(1, 1)
 spine2 = SpineNode(1, 2)
-dstnode = DstNode(LINE_RATE)
+dst1 = DstNode(LINE_RATE, 1)
 TICKS = 1000000
 
 for current_tick in range(0, TICKS):
-  srcnode.tick([spine1, spine2, spine3], current_tick)
-  spine1.tick([dstnode], current_tick)
-  spine2.tick([dstnode], current_tick)
-  dstnode.tick([], current_tick)
+  src1.tick([spine1, spine2], current_tick, [dst1])
+  spine1.tick([dst1], current_tick)
+  spine2.tick([dst1], current_tick)
+  dst1.tick([], current_tick)
+  if (current_tick == TICKS / 2):
+    print "Modified line rate for spine1"
+    spine1.modify_line_rate(0)
