729ead9a3fe18cb3e07abf70318706a98b2ada57
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-01-16 11:10:12 -0800

    Explain simplified detail algorithm

--- detail_srcnode.py
+++ detail_srcnode.py
@@ -12,6 +12,10 @@ class DeTailSrcNode(SrcNode):
   def tick(self, current_tick):
     for neighbor in numpy.random.permutation(self.neighbors):
       if ((len(self.neighbor_queue[neighbor]) > 0) and (neighbor.input_counters[self.id] < DeTailSrcNode.PAUSE_THRESHOLD)):
+        # send packets to neighbor only if:
+        # -- have packets for that neighbor
+        # -- that neighbor isn't overloaded with your packets
+        #    (i.e. your link to the neighbor is blocked)
         neighbor.recv(self.neighbor_queue[neighbor].pop(0))
 
   def recv(self, pkt):
@@ -19,7 +23,15 @@ class DeTailSrcNode(SrcNode):
     preferred_neighbors  = []
     for neighbor in self.neighbors:
       if (neighbor.input_counters[self.id] < DeTailSrcNode.PAUSE_THRESHOLD):
-        # Cheat (no pause messages)
+        # Adaptive load balancing
+        # In practice, this is the sequence of events
+        # 1. The neighbor has too many packets from self.
+        # 2. The neighbor issues a pause.
+        # 3. This causes self to pause its link to neighbor.
+        # 4. This causes the self-neighbor link queue to build up.
+        # 5. This causes self to move its traffic to other neighbors
+        # We hypersimplify by moving traffic to other neighbors
+        # as soon as step 1 occurs.
         preferred_neighbors.append(neighbor)
 
     if (len(preferred_neighbors) > 0):
