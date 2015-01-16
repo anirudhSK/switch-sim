67bfc393435c9d0445328a9fbf8f6d6f40c96798
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-01-16 10:39:44 -0800

    Make PAUSE_THRESHOLD a configurable parameter

--- detail_srcnode.py
+++ detail_srcnode.py
@@ -2,6 +2,7 @@ import numpy.random
 from src_node import SrcNode
 
 class DeTailSrcNode(SrcNode):
+  PAUSE_THRESHOLD = 5
   def __init__(self, t_line_rate, t_num_dsts, t_neighbors):
     SrcNode.__init__(self, t_line_rate, t_num_dsts, t_neighbors)
     self.neighbor_queue = dict()
@@ -10,14 +11,14 @@ class DeTailSrcNode(SrcNode):
 
   def tick(self, current_tick):
     for neighbor in numpy.random.permutation(self.neighbors):
-      if ((len(self.neighbor_queue[neighbor]) > 0) and (neighbor.input_counters[self.id] < 5)):
+      if ((len(self.neighbor_queue[neighbor]) > 0) and (neighbor.input_counters[self.id] < DeTailSrcNode.PAUSE_THRESHOLD)):
         neighbor.recv(self.neighbor_queue[neighbor].pop(0))
 
   def recv(self, pkt):
     pkt.last_hop = str(self)
     preferred_neighbors  = []
     for neighbor in self.neighbors:
-      if (neighbor.input_counters[self.id] < 5):
+      if (neighbor.input_counters[self.id] < DeTailSrcNode.PAUSE_THRESHOLD):
         # Cheat (no pause messages)
         preferred_neighbors.append(neighbor)
 
