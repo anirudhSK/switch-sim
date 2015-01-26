5590e0b09aa8e82c660a85da7eeafebc26ca3889
Anirudh Sivaraman <sk.anirudh@gmail.com>
2015-01-26 13:50:44 -0800

    Modify Vlb code to make DeTail and Vlb match up in the limit

--- vlb_srcnode.py
+++ vlb_srcnode.py
@@ -5,16 +5,17 @@ class VlbSrcNode(SrcNode):
 
   def __init__(self, t_line_rate, t_num_dsts, t_neighbors):
     SrcNode.__init__(self, t_line_rate, t_num_dsts, t_neighbors)
-    self.agg_pkt_queue = []
+    self.neighbor_queue = dict()
+    for neighbor in self.neighbors:
+      self.neighbor_queue[neighbor] = []
 
   def tick(self, current_tick):
-    assert(len(self.agg_pkt_queue) <= self.line_rate * len(self.neighbors));
     for neighbor in numpy.random.permutation(self.neighbors):
       for i in range(self.line_rate):
-        if (len(self.agg_pkt_queue) > 0) :
-          neighbor.recv(self.agg_pkt_queue.pop(0))
-    assert(len(self.agg_pkt_queue) == 0)
+        if (len(self.neighbor_queue[neighbor]) > 0) :
+          neighbor.recv(self.neighbor_queue[neighbor].pop(0))
 
   def recv(self, pkt):
     pkt.last_hop = str(self)
-    self.agg_pkt_queue.append(pkt)
+    chosen = numpy.random.choice(self.neighbors)
+    self.neighbor_queue[chosen].append(pkt)
